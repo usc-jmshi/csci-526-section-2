34e109dd5296931d4704f126461712a4e330701a
Justin Shi <jmshi@usc.edu>
2025-09-23 15:52:04 -0700

    Add mode select UI, include both game and level editor scenes in builds

---
+++ Assets/Scripts/ModeSelectUI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(UIDocument))]
+public class ModeSelectUI: MonoBehaviour {
+  private Button _switchModeBtn;
+
+  private void OnEnable() {
+    UIDocument uiDoc = GetComponent<UIDocument>();
+
+    _switchModeBtn = uiDoc.rootVisualElement.Q<Button>("switch-mode");
+
+    _switchModeBtn.RegisterCallback<ClickEvent>(OnClickEvent);
+  }
+
+  private void OnDisable() {
+    _switchModeBtn.UnregisterCallback<ClickEvent>(OnClickEvent);
+  }
+
+  private void Start() {
+    _switchModeBtn.text = GameManager.Instance.IsLevelEditor ? "Switch to Game" : "Switch to Level Editor";
+  }
+
+  private void OnClickEvent(ClickEvent evt) {
+    SceneManager.LoadScene(GameManager.Instance.IsLevelEditor ? "Game" : "LevelEditor");
+  }
+}
